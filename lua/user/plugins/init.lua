7c70dee9ef9c14d1e33a7382edb64c5d78a96d0b
Tashrif Sanil <7899547+tashrifsanil@users.noreply.github.com>
2022-08-16 06:43:23 -0400

    refactor: splitted out plugins configurations

---
+++ lua/user/plugins/init.lua
@@ -0,0 +1,44 @@
+return {
+  -- You can disable default plugins as follows:
+  -- ["goolord/alpha-nvim"] = { disable = true },
+
+  -- You can also add new plugins here as well:
+  -- Add plugins, the packer syntax without the "use"
+  -- { "andweeb/presence.nvim" },
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+
+  -- We also support a key value style plugin definition similar to NvChad:
+  -- ["ray-x/lsp_signature.nvim"] = {
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+  {
+    "folke/tokyonight.nvim",
+    as = "tokyonight",
+    config = function() vim.cmd [[colorscheme tokyonight]] end,
+  },
+  { "tpope/vim-abolish" },
+  { "tpope/vim-surround" },
+  { "tpope/vim-unimpaired" },
+  { "tpope/vim-repeat" },
+  { "machakann/vim-highlightedyank" },
+  { "jeetsukumaran/vim-indentwise" },
+  { "mg979/vim-visual-multi" },
+  { "michaeljsmith/vim-indent-object" },
+  { "ggandor/lightspeed.nvim" },
+  -- Sticky scroll similar to VSCode's implementation
+  -- see: https://www.youtube.com/shorts/znue7UqdUf0
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    after = "nvim-treesitter",
+    config = function() require("treesitter-context").setup() end,
+  },
+}
