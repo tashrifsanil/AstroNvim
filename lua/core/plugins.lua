8f4d5ee82a0e6ae7d471fdff89ddf4f0e973b662
Tashrif Sanil <7899547+tashrifsanil@users.noreply.github.com>
2022-10-20 14:00:14 -0400

    fix: force fzf_native usage

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -188,10 +188,10 @@ local astro_plugins = {
   },
 
   -- Fuzzy finder syntax support
-  [("nvim-telescope/telescope-%s-native.nvim"):format(vim.fn.has "win32" == 1 and "fzy" or "fzf")] = {
+  ["nvim-telescope/telescope-fzf-native.nvim"] = {
     after = "telescope.nvim",
-    run = vim.fn.has "win32" ~= 1 and "make" or nil,
-    config = function() require("telescope").load_extension(vim.fn.has "win32" == 1 and "fzy_native" or "fzf") end,
+    run = 'make',
+    config = function() require("telescope").load_extension("fzf") end,
   },
 
   -- Git integration
